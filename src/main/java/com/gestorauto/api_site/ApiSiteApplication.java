8a5076643cf60ac8a3144c807622d45eb4523d93
GaTrani <gabrieltrani02@gmail.com>
2025-01-21 10:23:43 -0300

    :tada: Commit Initial

---
+++ src/main/java/com/gestorauto/api_site/ApiSiteApplication.java
@@ -0,0 +1,13 @@
+package com.gestorauto.api_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSiteApplication.class, args);
+	}
+
+}
